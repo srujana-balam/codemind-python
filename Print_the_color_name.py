7dc46c5451f6fc42cf9776757c312dfe3a51bdc7
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-08-18 11:13:15 +0530

    n=input() if (n=='V' or n=='v'):     print("Violet") elif (n=='I' or n=='i'):     print("Indigo") elif (n=='B' or n=='b'):     print("Blue") elif (n=='G' or n=='g'):     print("Green") elif (n=='Y' or n=='y'):     print("Yellow") elif (n=='O' or n=='o'):     print("Orange") elif (n=='R' or n=='r'):     print("Red") else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+n=input()
+if (n=='V' or n=='v'):
+    print("Violet")
+elif (n=='I' or n=='i'):
+    print("Indigo")
+elif (n=='B' or n=='b'):
+    print("Blue")
+elif (n=='G' or n=='g'):
+    print("Green")
+elif (n=='Y' or n=='y'):
+    print("Yellow")
+elif (n=='O' or n=='o'):
+    print("Orange")
+elif (n=='R' or n=='r'):
+    print("Red") 
+else:
+    print(-1)
